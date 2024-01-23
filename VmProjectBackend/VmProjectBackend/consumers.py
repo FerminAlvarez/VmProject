cb8755cb710cccc59120c6bd3dcd49703c4f55f9
Fermin Alvarez <ferminalvarez.dev@gmail.com>
2024-01-23 04:29:20 -0300

    Websocket VM endpoint realtime

--- VmProjectBackend/VmProjectBackend/consumers.py
+++ VmProjectBackend/VmProjectBackend/consumers.py
@@ -1,22 +1,26 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from .ssh.remote_vm_realtime import  execute_vm_command, decode, get_welcome_message
 
 class MyConsumer(WebsocketConsumer):
 
     def connect(self):
         print('Connection established')
         self.accept()
+        self.send(get_welcome_message())
 
     def disconnect(self, close_code):
         print('Disconnected')
-        pass
+        self.close()
+    
+    def send_response(self, response):
+        self.send(response)
 
     def receive(self, text_data):
         print('Received Message')
 
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        self.send(text_data=json.dumps({
-            'message':message
-        }))
+        command = text_data_json['command']
+        
+        for line in execute_vm_command(command):
+            self.send_response(line)
--- VmProjectBackend/VmProjectBackend/ssh/remote_vm_realtime.py
+++ VmProjectBackend/VmProjectBackend/ssh/remote_vm_realtime.py
@@ -1,31 +1,42 @@
 from django.conf import settings
-import time
 import re
 import paramiko
+import time
+
 
 VMHOST=settings.VMHOST
 VMUSER=settings.VMUSER
 VMPASSWORD=settings.VMPASSWORD
 VMPRIVATE_KEY_PATH=settings.VMPRIVATE_KEY_PATH
 
+BUFFER_SIZE = 4096
+
+EXPECTED_TERMINATING_LINE = "vm@vmproject:~$"
+CONTINUE_PROMPT = "Y/n"
+
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 private_key = paramiko.RSAKey(filename=VMPRIVATE_KEY_PATH, password=VMPASSWORD)
 client.connect(VMHOST, username=VMUSER, pkey=private_key)
 channel = client.invoke_shell()
+channel.settimeout(5)
 
 def execute_vm_command( command):
         print(command)
         try:
             channel.send(command + '\n')
-            
-            while not channel.recv_ready():
-                time.sleep(0.1)
                 
-            out = channel.recv(99999)
-            cleaned_output = decode(out)
+            while True:
+                out = channel.recv(BUFFER_SIZE)
+                cleaned_output = decode(out)
+                yield cleaned_output
+                
+                if EXPECTED_TERMINATING_LINE in cleaned_output or CONTINUE_PROMPT in cleaned_output:
+                    break
+                
+        except TimeoutError as e:
+            print("The operation timed out")
             
-            return cleaned_output
         except Exception as e:
             print("There was an error", e)
             return str(e)
@@ -39,3 +50,9 @@ def decode( text):
     decoded_output = text.decode('utf-8')
     ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
     return  ansi_escape.sub('', decoded_output)
+
+def get_welcome_message():
+    out = channel.recv(BUFFER_SIZE)
+    cleaned_output = decode(out)
+    print(cleaned_output)
+    return cleaned_output
