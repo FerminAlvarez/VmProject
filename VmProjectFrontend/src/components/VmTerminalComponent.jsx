005b097cfbc72f79cbc0b01aa5b20c02b890f96d
Fermin Alvarez <ferminalvarez.dev@gmail.com>
2024-01-24 03:15:46 -0300

    Fix text alignment in code block

--- VmProjectFrontend/src/components/VmTerminalComponent.jsx
+++ VmProjectFrontend/src/components/VmTerminalComponent.jsx
@@ -31,7 +31,7 @@ const VmTerminalComponent = ({ token }) => {
                 </button>
             </div>
             {isWebSocketConnected &&
-                <code className="flex flex-col text-sm sm:text-base text-left items-start space-x-4 bg-gray-800 text-white rounded-lg p-4 pl-6 overflow-auto h-96">
+                <code className="flex flex-col text-sm sm:text-base text-left items-start bg-gray-800 text-white rounded-lg p-4 pl-6 overflow-auto h-96">
                     {webSocketMessages.map((message, index) => (
                         message.includes('vm@vmproject:') || message.includes('Y/n')  ?
                             <span key={index} className='whitespace-pre-wrap text-green-600 inline-flex'>{message}
